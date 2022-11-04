9bf55a068ad1831f2b979a0e24df9c0ba7450fa5
Yoon <yoondev93@gmail.com>
2022-11-05 01:16:43 +0900

    feature: 템플릿 메서드 패턴 - 적용1

---
+++ advanced/src/main/java/hello/advanced/trace/template/AbstractTemplate.java
@@ -0,0 +1,30 @@
+package hello.advanced.trace.template;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public abstract class AbstractTemplate<T> {
+
+  private final LogTrace trace;
+
+  public T execute(String message) {
+    TraceStatus status = null;
+    try {
+      status = trace.begin(message);
+
+      T result = call();
+
+      trace.end(status);
+
+      return result;
+    } catch (Exception e) {
+      trace.exception(status, e);
+      throw e;
+    }
+  }
+
+  protected abstract T call();
+
+}
